da9d624d3d7e2e704ed0401b0080966510e8ae32
YvonneJohansson <yvonnemaria.johansson@gmail.com>
2023-06-02 14:44:04 +0100

    Extending the quick analysis

--- yvonne_analysis/YJ_postproc_singleMouse_singleSession.py
+++ yvonne_analysis/YJ_postproc_singleMouse_singleSession.py
@@ -6,6 +6,7 @@ sys.path.insert(1, 'C:\\Users\\Yvonne\\Documents\\YJ_photometry\\yvonne_analysis
 
 import pandas as pd
 from yvonne_basic_functions import *
+from YJ_plotting import *
 
 # main_directory = r'Z:\users\Yvonne\photometry_2AC'
 main_directory = 'Z:\\users\\Yvonne\\photometry_2AC\\'
@@ -13,19 +14,14 @@ main_directory = 'Z:\\users\\Yvonne\\photometry_2AC\\'
 # function by YJ to analyse a single session of a single mouse
 
 if __name__ == '__main__':
-    mouse_ids = ['TS3']
-    dates = ['20230303']
+    mice = ['TS16'] # ['TS19','TS20','TS21','TS22']
+    dates = ['20230512'] #,'20230220','20230227','20230307','20230301','20230509'] # '20230511', '20230512', '20230513', '20230510', '20230511','20230512'
     all_experiments = get_all_experimental_records()
 
-    for mouse_id in mouse_ids:
+    for mouse in mice:
         for date in dates:
             experiments_to_process = all_experiments[
-                (all_experiments['date'] == date) & (all_experiments['mouse_id'] == mouse_id)]
-            # print('Experiments to process: ' + experiments_to_process)
-            analyse_this_experiment(experiments_to_process, main_directory)
+                (all_experiments['date'] == date) & (all_experiments['mouse_id'] == mouse)]
+            data = analyse_this_experiment(experiments_to_process, main_directory)
 
-            #data = get_SessionData(main_directory, mouse_id, date, 'right', 'tail')
-            #plt.plot(data.choice.contra_data.time_points,data.choice.contra_data.mean_trace)
-            #plt.xlim([-2, 3])
-            #plt.text(-2, -0.6, data.choice.contra_data.title, ha='center', va='center')
-            #plt.show()
+
